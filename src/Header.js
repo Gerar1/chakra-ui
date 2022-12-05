6656c2e7cbe394df12c081a776c5b2a3fb19e7a1
Gerar <gerbas.cc@gmail.com>
2022-12-05 12:38:55 -0300

    chakra

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Image } from "@chakra-ui/image"
+
+const Example = () => (
+  <Image
+    src="https://i.pinimg.com/736x/25/b8/f1/25b8f17cc40a606acfdaf5a8d3726fb0.jpg"
+    fallbackSrc="placeholdit.com/400x400"
+    alt="joven en bicicleta"
+  />
+);
+
+export default Example;
